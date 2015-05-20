9cb0c9e94db1a072ecec8e85935b067744ac3760
equipo1csd <rdelacalleja@epicor.com>
2015-05-20 13:16:04 -0300

    mostrar palabra1

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 
 get '/' do
-
+@@mensaje = "f _ _ _ _ _"
+	erb :index	
 end
---
+++ features/step_definitions/my_steps.rb
@@ -0,0 +1,9 @@
+Given(/^abrir la aplicacion$/) do
+  visit '/'
+end
+
+Then(/^debo ver "(.*?)"$/) do |text|
+  "f _ _ _ _ _"
+end
+
+
